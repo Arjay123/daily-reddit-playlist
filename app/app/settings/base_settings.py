61f8d6e8addc96ff64279ea8bc43c2d4d0fffffc
arjaynguyen <arjaynguyen123@hotmail.com>
2017-11-29 12:18:21 -0800

    remove sample app, create website app

--- app/app/settings/base_settings.py
+++ app/app/settings/base_settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'sample.apps.SampleConfig',
+    'website.apps.WebsiteConfig',
     'webpack_loader'
 ]
 
