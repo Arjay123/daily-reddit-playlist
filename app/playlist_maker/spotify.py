e2ac5c1cbc4e55c170001c93fce8b7ba923b5350
arjaynguyen <arjaynguyen123@hotmail.com>
2017-12-05 14:04:15 -0800

    get spotify instance and search implemented

--- app/playlist_maker/spotify.py
+++ app/playlist_maker/spotify.py
@@ -1,9 +1,11 @@
 import requests
-
+import spotipy
 
 def refresh_access_token(refresh_token, client_id, client_secret):
     """
     Takes in spotify refresh token and returns new access token
+
+    TODO - Better error handling
     """
     data = {
         'grant_type': 'refresh_token',
@@ -19,5 +21,29 @@ def refresh_access_token(refresh_token, client_id, client_secret):
     if response.status_code == requests.codes.ok:
         return response.json()['access_token']
     else:
-        # TODO - Better error handling
         print response.content
+
+
+def create_spotify_instance(access_token):
+    """
+    Returns Spotify instance using access_token
+
+    TODO - Better error handling, maybe use logger
+    """
+    try:
+        spotify_instance = spotipy.Spotify(auth=access_token)
+        spotify_instance.current_user()
+        return spotify_instance
+    except spotipy.client.SpotifyException:
+        print "error"
+
+
+def search_track(spotify, artist, trackname):
+    """
+    """
+    query = artist + ' - ' + trackname
+    result = spotify.search(query)
+
+    if result['tracks']['total'] > 0:
+        # TODO - Add checks to match artist and trackname
+        return result['tracks']['items'][0]['id']
