56ca0e9d11e11edc5988aba41c23bd9c50c8f5bf
arjaynguyen <arjaynguyen123@hotmail.com>
2017-12-04 17:26:54 -0800

    implemented retrieve_submissions

--- app/playlist_maker/reddit.py
+++ app/playlist_maker/reddit.py
@@ -1,5 +1,7 @@
 import praw
 import time
+import requests
+
 from datetime import date
 from datetime import timedelta
 
@@ -8,12 +10,20 @@ from playlist_maker.reddit_secrets import CLIENT_SECRET
 from playlist_maker.reddit_secrets import CLIENT_ID
 
 
+# List of supported subreddits
 SUBREDDITS = ['hiphopheads']
+
+# Disable SNIMissing Warnings, InsecurePlatformWarnings due to use older
+# version of python w/ urllib3
+requests.packages.urllib3.disable_warnings()
+
 
 def create_reddit_instance():
     """
     Creates and returns a reddit instance w/ credentials from reddit_secrets
     script
+
+    #TODO - check reddit instance is valid before returning
     """
     return praw.Reddit(client_id=CLIENT_ID,
                        client_secret=CLIENT_SECRET,
@@ -22,18 +32,35 @@ def create_reddit_instance():
                        username='daily-reddit-playlis')
 
 
-def retrieve_submissions_from_subreddit(reddit, subreddit):
+def retrieve_submissions_from_subreddit(reddit, subreddit_name):
     """
     Retrieves all submissions for a specific subreddit from previous day
 
     Args:
         reddit - reddit instance
-        subreddit - title of subreddit as a string
+        subreddit_name - name of subreddit as a string
 
     Returns:
         List of submissions
+
+
+    # TODO - check subreddit is valid before retrieving submissions
     """
-    pass
+    subreddit = reddit.subreddit(subreddit_name)
+    start_timestamp, end_timestamp = get_unix_timestamps_prev_day()
+
+    submissions = subreddit.submissions(start=start_timestamp,
+                                        end=end_timestamp)
+
+    results = []
+    for sub in submissions:
+        results.append({
+            'title': sub.title,
+            'url': sub.url,
+            'created': sub.created_utc
+        })
+
+    return results
 
 
 def get_unix_timestamps_prev_day():
